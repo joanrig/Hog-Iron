c689a09f036b95abc8a36a830e44c6955ecb6509
Lizzie <lchan217@gmail.com>
2019-04-30 14:21:57 -0500

    students controller methods

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,42 @@
 class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+  def new
+    @student = Student.new
+  end
+  def create
+    @student.new(student_params)
+    if @student.valid?
+      @student.save
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+  def show
+    @student = Student.find(params[:id])
+  end
+  def edit
+    @student = Student.find(params[:id])
+  end
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      redirect_to student_path(@student)
+    else
+      render :edit
+    end
+  end
+  def destroy
+    Student.find(params[:id]).destroy
+    redirect_to students_path
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :heritage, :alive, :patronus)
+    #left out age and pet for now
+  end
 end
